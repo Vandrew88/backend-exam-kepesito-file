e7bd917de829375174e9d624796d55b97d72491e
Vandrew88 <andras.vasmatics@gmail.com>
2021-04-29 12:15:44 +0200

    Implement method

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,24 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+
+        try{
+            Files.write(Paths.get(output), contacts.entrySet()
+                    .stream()
+                    .map(stringStringEntry -> String.format("%s: %s", stringStringEntry.getKey(), stringStringEntry.getValue()))
+                    .collect(Collectors.joining(System.lineSeparator())).getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
